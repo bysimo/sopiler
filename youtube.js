286db253e81c255b7dd29af30f1434d1e5019833
simopunkc <simopunkc@student.ub.ac.id>
2021-10-02 13:51:25 +0700

    javascript convert url to embedded video html

--- youtube.js
+++ youtube.js
@@ -1,1 +1,30 @@
-
+function convert_url(){
+    var url=document.getElementById('url').value;
+    if(url!=null){
+        var regxwww=/^(?:http|https):\/\/[a-zA-Z0-9=+_./-]+[.]+/;
+        if(url.match(regxwww)){
+            var regx_yutub=/^((?:http|https):\/\/(?:www|m)\.youtube\.com)\/watch\?v\=([a-zA-Z0-9]{1,15})/;
+            var hasilrgx=url.match(regx_yutub);
+            if(hasilrgx){
+                url=hasilrgx[2];
+            }
+            regx_yutub=/^(?:http|https):\/\/youtu\.be\/([a-zA-Z0-9]{1,15})/;
+            hasilrgx=url.match(regx_yutub);
+            if(hasilrgx){
+                url=hasilrgx[1];
+            }
+            return url;
+        }else{
+            alert("Incorrect URL address");
+        }
+    }
+}
+function formatEmbed(str){
+    var embed='<iframe src="https://www.youtube.com/embed/'+str+'" width="480" height="360" title="Embedded Media"></iframe>';
+    return embed;
+}
+function embedVideo(){
+    var idvideo=convert_url();
+    var mulaiEmbed=formatEmbed(idvideo);
+	document.getElementById("hasil").value=mulaiEmbed;
+}
